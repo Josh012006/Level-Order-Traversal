8dfb08ff7c8f3b3d85c766441232e6350dc9433b
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2024-12-06 13:09:29 -0500

    Update algorithm.cpp

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,25 +1,37 @@
-void levelOrder(Node * root){
-      //Write your code here
-        if(root == NULL) return;
-
-        queue<Node*> q; // Create a queue to store nodes for traversal
-        q.push(root);   // Start with the root node
-
-        while(!q.empty()){
-            Node* current = q.front(); // Get the front node in the queue
-            q.pop();                   // Remove the front node from the queue
-
-            cout << current->data << " "; // Print the data of the current node
-
-            // Enqueue the left child if it exists
-            if(current->left != NULL) {
-                q.push(current->left);
-            }
-
-            // Enqueue the right child if it exists
-            if(current->right != NULL) {
-                q.push(current->right);
-            }
+class Node{
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d){
+            data = d;
+            this->left = NULL;
+            this->right = NULL;
         }
-  
+};
+
+
+void levelOrder(Node * root){
+    if(root == NULL) return;
+
+	queue<Node*> q; // Create a queue to store nodes for traversal
+	q.push(root);   // Start with the root node
+
+	while(!q.empty()){
+		Node* current = q.front(); // Get the front node in the queue
+		q.pop();                   // Remove the front node from the queue
+
+		cout << current->data << " "; // Print the data of the current node
+
+		// Enqueue the left child if it exists
+		if(current->left != NULL) {
+			q.push(current->left);
+		}
+
+		// Enqueue the right child if it exists
+		if(current->right != NULL) {
+			q.push(current->right);
+		}
 	}
+
+}
